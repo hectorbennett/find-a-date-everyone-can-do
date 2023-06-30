abebd5a0863c8f7c035ce290bcc310917a9650b1
Hector <hectorbennett@outlook.com>
2023-06-30 08:59:33 +0100

    Add a 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import Container from "@/components/container";
+import { PageHeader } from "@/components/page_header";
+import { SimpleGrid, Title } from "@mantine/core";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <PageHeader />
+      <Container>
+        <SimpleGrid spacing="md" breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+          <Title>Page not found :(</Title>
+        </SimpleGrid>
+      </Container>
+    </>
+  );
+}
