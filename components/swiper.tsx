e559b42e1af5414f12945273da6e39fea3667231
Hector <hectorbennett@outlook.com>
2023-07-10 09:39:48 +0100

    fix calendar swiping on mobile

--- components/swiper.tsx
+++ components/swiper.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { Box } from "@mantine/core";
 import { useElementSize } from "@mantine/hooks";
 import { useSpring, animated } from "@react-spring/web";
@@ -11,12 +11,12 @@ interface SwiperProps {
 }
 
 export default function Swiper({ index, setIndex, getContent }: SwiperProps) {
-  // const [index, setIndex] = useState(0);
   const { ref, width } = useElementSize();
+  const prevWidth = useRef(0);
   const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
 
   const bind = useDrag(
-    ({ down, movement: [mx], tap }) => {
+    ({ down, movement: [mx], tap, velocity: [vx] }) => {
       if (tap) {
         // ignore tap events
         return;
@@ -26,19 +26,23 @@ export default function Swiper({ index, setIndex, getContent }: SwiperProps) {
         api.start({ x: mx + -index * width });
         return;
       }
+      if (Math.abs(mx) < width / 2 && vx < 0.2) {
+        // if we've only moved a little bit and velocity is low, cancel and stay on the same
+        // item
+        api.start({ x: -index * width });
+        return;
+      }
 
-      // otherwise, decide whether to stay on the same item (if we haven't dragged far enough)
-      // or to move to the next one
-      if (mx > 10) {
+      // otherwise, decide whether to move left or right
+      if (mx > 20) {
         dragRight();
-      } else if (mx < -10) {
+      } else if (mx < -20) {
         dragLeft();
       } else {
-        // stay
         api.start({ x: -index * width });
       }
     },
-    { filterTaps: true }
+    { filterTaps: true, axis: "x" }
   );
 
   const dragLeft = () => {
@@ -50,12 +54,14 @@ export default function Swiper({ index, setIndex, getContent }: SwiperProps) {
   };
 
   useEffect(() => {
-    api.start({ x: -index * width, immediate: true });
-  }, [width]);
-
-  useEffect(() => {
-    api.start({ x: -index * width });
-  }, [index]);
+    if (width !== prevWidth.current) {
+      /* if we've resized the window, do not animate */
+      api.start({ x: -index * width, immediate: true });
+      prevWidth.current = width;
+    } else {
+      api.start({ x: -index * width });
+    }
+  }, [api, index, width]);
 
   return (
     <Box
@@ -69,7 +75,7 @@ export default function Swiper({ index, setIndex, getContent }: SwiperProps) {
         {...bind()}
         style={{
           display: "flex",
-          // touchAction: "none",
+          touchAction: "pan-y",
           userSelect: "none",
           x,
           y,
@@ -84,7 +90,7 @@ export default function Swiper({ index, setIndex, getContent }: SwiperProps) {
           }}
         >
           {[index - 1, index, index + 1].map((i, j) => (
-            <Box style={{ width }} key={j}>
+            <Box style={{ width, height: i !== index ? 0 : undefined }} key={j}>
               {getContent(i)}
             </Box>
           ))}
