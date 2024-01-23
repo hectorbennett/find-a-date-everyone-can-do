e4d8f0dbbd4d2f2f2f3c1bfd5e503b299a23d07d
Hector <hectorbennett@outlook.com>
2024-01-23 08:28:39 +0000

    jsdoc lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import Card from "@/components/card";
 import CreateEvent from "@/components/create_event";
 import RecentEvents from "@/components/cards/recent_events";
 
+/**
+ * This component renders the 'New event' title component that exists on the home screen for creating a new event.
+ * @returns A React element that renders a new event string.
+ */
 function TitleAndInfo() {
   return (
     <Card>
