c77931be987f232b4095668a027b68bdcd084e1f
Hector <hectorbennett@outlook.com>
2024-01-25 08:45:43 +0000

    dev

--- stories/layout.stories.tsx
+++ stories/layout.stories.tsx
@@ -2,16 +2,17 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Layout from "../components/layout";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-const meta: Meta<typeof Layout> = {
+const meta = {
   component: Layout,
   tags: ["autodocs"],
-  argTypes: {},
-};
+} satisfies Meta<typeof Layout>;
 
 export default meta;
-type Story = StoryObj<typeof Layout>;
+type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Example: Story = {
-  args: {},
+  args: {
+    children: "Hello World",
+  },
 };
--- stories/swiper.stories.tsx
+++ stories/swiper.stories.tsx
@@ -1,7 +1,7 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import Swiper from "../components/swiper";
 import { ReactNode } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { Box } from "@mantine/core";
+import Swiper from "../components/swiper";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Swiper> = {
