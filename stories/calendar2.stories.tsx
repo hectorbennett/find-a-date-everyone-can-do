8272b98383c0699430d15eedd0a95bab8dabb572
Hector <hectorbennett@outlook.com>
2024-01-25 08:51:14 +0000

    dev

--- stories/calendar2.stories.tsx
+++ stories/calendar2.stories.tsx
@@ -25,30 +25,3 @@ export const Primary: Story = {
     }),
   },
 };
-
-// import type { Meta, StoryObj } from "@storybook/react";
-// import Calendar from "../components/calendar2";
-
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-// const meta: Meta<typeof Calendar> = {
-//   component: Calendar,
-//   tags: ["autodocs"],
-//   argTypes: {},
-// };
-
-// export default meta;
-// type Story = StoryObj<typeof Calendar>;
-
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-// export const Example: Story = {
-//   args: {
-//     getDayProps: (date) => ({
-//       isSelected: Math.random() < 0.5,
-//       isToday: false,
-//       isInPast: false,
-//       onClick: () => console.log("click", date),
-//       selectionCount: 3,
-//       heat: Math.random(),
-//     }),
-//   },
-// };
